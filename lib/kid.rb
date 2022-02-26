3902b6cc55cdae8d8f588154571801f52ca88d9d
Olha Gruzglina <olga.zhmura@gmail.com>
2022-02-26 15:02:33 -0500

    commit tree 1eab9e9e099a61f31f8a6511e15bf62745e611bb

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,17 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance.rb'
+
+class Kid
+    # extend MetaDancing
+    # include Dance
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+  
+    attr_reader :name
+  
+    def initialize(name)
+      @name = name
+    end
+    metadata
+  end
